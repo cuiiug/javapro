2e84643c46d625a2eaad6ba5c177a885ea5a282f
cuiiug <myjava_cn@126.com>
2017-12-07 20:16:02 +0800

    commit

--- src/main/java/cn/hui/javapro/App.java
+++ src/main/java/cn/hui/javapro/App.java
@@ -1,13 +1,42 @@
 package cn.hui.javapro;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    
+    public static void main(String[] args) {
+        testArrayListRemoveException();
+        System.out.println("over.");
+    }
+
+    public static void testArrayListRemoveException(){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(112);
+        list.add(2);
+        list.add(22);
+        list.add(23);
+        list.add(24);
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.addAll(list);
+        Iterator<Integer> it = list.iterator();
+        //第一种删除
+        for(;it.hasNext();){
+            System.out.println(it.next());
+            it.remove();
+        }
+        list2.remove(1);
+        // 第二种删除
+        for(int i = 0;i<list2.size();i++){
+            Integer  s = list2.get(i);
+            list.remove(s);
+        }
     }
 }
--- src/main/java/cn/hui/javapro/list/arraylist/MeArrayList.java
+++ src/main/java/cn/hui/javapro/list/arraylist/MeArrayList.java
@@ -1,10 +1,14 @@
 package cn.hui.javapro.list.arraylist;
 
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.RandomAccess;
+import java.util.function.Consumer;
 
 public class MeArrayList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
 
@@ -17,28 +21,43 @@ public class MeArrayList<E> implements List<E>, RandomAccess, Cloneable, java.io
      * 
      */
     private static final long serialVersionUID = 1L;
-    
-     /**
-     * List中元素个数
-     *
-     */
+
+    /**
+    * List中元素个数
+    *
+    */
     private int size;
-     /**
-     * 返回List中元素个数.
+
+    /**
+     * transient 不参与序列化，只能修饰成员变量，不能修饰方法、类、局部变量
+     * 
      */
+    transient Object[] elementData; // non-private to simplify nested class access
+
+    /**
+     * 暂：改List size被修改的次数
+     */
+    protected transient int modCount = 0;
+
+    /**
+    * 返回List中元素个数.
+    */
     public int size() {
         return size;
     }
-  
+
     /**
-     * 
+     * 返回List中是否没有元素
      */
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
+    /**
+     * 返回List是否包含 o ，调用indexof 方法
+     */
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
     public Iterator<E> iterator() {
@@ -101,8 +120,24 @@ public class MeArrayList<E> implements List<E>, RandomAccess, Cloneable, java.io
         return null;
     }
 
+    /**
+     * 返回List中第一个出现 o 的位置，如果没有 返回-1
+     */
     public int indexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     public int lastIndexOf(Object o) {
@@ -119,6 +154,71 @@ public class MeArrayList<E> implements List<E>, RandomAccess, Cloneable, java.io
 
     public List<E> subList(int fromIndex, int toIndex) {
         return null;
+    }
+
+    private class Itr implements Iterator<E> {
+        int cursor; // 下一个元素的index
+        int lastRet = -1; // 最后一个元素的index；没有返回-1
+        int expectedModCount = modCount;
+
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        public E next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size) {
+                throw new NoSuchElementException();
+            }
+            Object[] elementData = MeArrayList.this.elementData;
+            if (i >= elementData.length)
+                throw new ConcurrentModificationException();
+            cursor = i + 1;
+            return (E) elementData[lastRet = i];
+        }
+
+        public void remove() {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+            checkForComodification();
+
+            try {
+                MeArrayList.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        public void forEachRemaining(Consumer<? super E> consumer) {
+            Objects.requireNonNull(consumer);
+            final int size = MeArrayList.this.size;
+            int i = cursor;
+            if (i >= size) {
+                return;
+            }
+            final Object[] elementData = MeArrayList.this.elementData;
+            if (i >= elementData.length) {
+                throw new ConcurrentModificationException();
+            }
+            while (i != size && modCount == expectedModCount) {
+                consumer.accept((E) elementData[i++]);
+            }
+            // update once at end of iteration to reduce heap write traffic
+            cursor = i;
+            lastRet = i - 1;
+            checkForComodification();
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
     }
 
 }
